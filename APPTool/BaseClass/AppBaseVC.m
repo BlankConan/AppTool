81285e73bd25960a24c58ceed75ab310893d70c7
BlankConan <lgy_975@126.com>
2018-11-10 15:36:24 +0800

    Erroe

---
+++ APPTool/BaseClass/AppBaseVC.m
@@ -0,0 +1,32 @@
+//
+//  AppBaseVC.m
+//  APPTool
+//
+//  Created by liugangyi on 2018/11/10.
+//  Copyright © 2018年 liu gangyi. All rights reserved.
+//
+
+#import "AppBaseVC.h"
+
+@interface AppBaseVC ()
+
+@end
+
+@implementation AppBaseVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
