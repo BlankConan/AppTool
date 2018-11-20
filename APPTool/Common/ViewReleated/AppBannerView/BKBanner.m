29cb54b4d52119fe6896f72eedd53fba6f5c4e35
BlankConan <lgy_975@126.com>
2018-11-20 12:27:12 +0800

    手动轮播

--- APPTool/Common/ViewReleated/AppBannerView/BKBanner.m
+++ APPTool/Common/ViewReleated/AppBannerView/BKBanner.m
@@ -15,14 +15,16 @@ static NSString *cellID = @"BKBannerCell";
 
 @interface BKBanner ()
 <
+UIScrollViewDelegate,
 UICollectionViewDelegate,
 UICollectionViewDataSource,
-UIScrollViewDelegate
+UICollectionViewDelegateFlowLayout
 >
 
 @property (nonatomic, strong) BKTimer *timer;
 @property (nonatomic, strong) UICollectionView *container;
 @property (nonatomic, assign) BOOL isFirstLoad;
+@property (nonatomic, assign) BOOL isDraging;
 @end
 
 @implementation BKBanner
@@ -46,15 +48,21 @@ UIScrollViewDelegate
     return self;
 }
 
-#pragma mark - Delegate
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    
-}
+#pragma mark - ScrollerView delegate
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    
+    [self resetContentOffset];
 }
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    _isDraging = YES;
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView {
+    _isDraging = NO;
+}
+
+#pragma mark - CollectionView Delegate
 
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     
@@ -66,7 +74,7 @@ UIScrollViewDelegate
         // oxxxo 三个元素 两个空位
         if (indexPath.row == 0) { // 第一个
             cell.model = [self.bannerModelArray lastObject];
-        } else if (indexPath.row == self.bannerModelArray.count - 1) {
+        } else if (indexPath.row == self.bannerModelArray.count + 1) {
             cell.model = [self.bannerModelArray firstObject];
         } else {
             cell.model = self.bannerModelArray[indexPath.row - 1];
@@ -83,20 +91,56 @@ UIScrollViewDelegate
     return _bannerModelArray.count + 2;
 }
 
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 0.0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0.0;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(self.bounds.size.width, self.bounds.size.height);
+}
+
 
 - (void)willMoveToSuperview:(UIView *)newSuperview {
+    [self.container setFrame:self.bounds];
     [self.container reloadData];
+    if (self.bannerModelArray.count > 1) {
+        [self.container scrollToItemAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0] atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
+    }
 }
 
 #pragma mark Target Action
 
 - (void)loopAction {
+    if (_isDraging) return;
     
 }
 
 #pragma mark - Assistan method
 
-- (void)reloadData {
+- (void)resetContentOffset {
+    
+    CGFloat width = CGRectGetWidth(self.bounds);
+    CGPoint contentOffset = self.container.contentOffset;
+    NSInteger currentIndex = contentOffset.x / width;
+    // 原理，元素对应，更新下标(x:元素 o:填充物)
+    // 0 1 2 3 4 下标
+    // 9 5 8 9 5 元素内容
+    // o x x x o
+    // 切换下标，重置偏移量
+    if (currentIndex == self.bannerModelArray.count + 1) {
+        currentIndex = 1;
+    } else if (currentIndex == 0) {
+        currentIndex = self.bannerModelArray.count;
+    }
+    
+    [self.container setContentOffset:CGPointMake(currentIndex * width, 0) animated:NO];
+}
+
+- (void)updatePageControl {
     
 }
 
@@ -123,17 +167,14 @@ UIScrollViewDelegate
     
     if (!_container) {
         UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
-        layout.itemSize = CGSizeMake(self.bounds.size.width, self.bounds.size.height);
         layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-        layout.minimumInteritemSpacing = 0;
-        layout.minimumLineSpacing = 0;
-        
         _container = [[UICollectionView alloc] initWithFrame:self.bounds collectionViewLayout:layout];
         _container.backgroundColor = [UIColor whiteColor];
         _container.delegate = self;
         _container.dataSource = self;
         _container.pagingEnabled = YES;
         _container.bounces = NO;
+        _container.showsHorizontalScrollIndicator = NO;
         [_container registerClass:[BKBannerCell class] forCellWithReuseIdentifier:cellID];
         [self addSubview:_container];
     }
